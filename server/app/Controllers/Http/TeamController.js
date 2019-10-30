af043922f001f45bc9337407f92d413064f8809c
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-10-29 23:11:59 -0300

    feat(store): TeamController

--- server/app/Controllers/Http/TeamController.js
+++ server/app/Controllers/Http/TeamController.js
@@ -1,7 +1,7 @@
-'use strict'
+"use strict";
 
 class TeamController {
-    /**
+  /**
    * Show a list of all teams.
    * GET teams
    *
@@ -10,11 +10,30 @@ class TeamController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ auth }) {
-    const teams = await auth.user.teams().fetch()
+  async index({ auth }) {
+    const teams = await auth.user.teams().fetch();
 
-    return teams
+    return teams;
+  }
+
+  /**
+   * Create/save a new team.
+   * POST teams
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request, auth }) {
+    const data = request.only(["name"]);
+
+    const team = await auth.user.teams().create({
+      ...data,
+      user_id: auth.user.id
+    });
+
+    return team;
   }
 }
 
-module.exports = TeamController
+module.exports = TeamController;
