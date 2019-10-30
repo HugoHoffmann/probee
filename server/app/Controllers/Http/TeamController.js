43f8df1de77016de7a3a63154e146c7b738e050f
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-10-29 23:11:11 -0300

    feat(index): TeamController

--- server/app/Controllers/Http/TeamController.js
+++ server/app/Controllers/Http/TeamController.js
@@ -1,6 +1,20 @@
 'use strict'
 
 class TeamController {
+    /**
+   * Show a list of all teams.
+   * GET teams
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ auth }) {
+    const teams = await auth.user.teams().fetch()
+
+    return teams
+  }
 }
 
 module.exports = TeamController
