47dee4e88f68aace75dc3c9c59038d1d6fe16068
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-10-29 21:15:21 -0300

    feat(store): UserController

--- server/app/Controllers/Http/UserController.js
+++ server/app/Controllers/Http/UserController.js
@@ -1,6 +1,15 @@
 'use strict'
 
+const User = use('App/Models/User');
 class UserController {
+    async store({ request, response, auth }){
+        const data = request.only([ 'name', 'email', 'password' ])
+        const user = await User.create(data)
+
+        const token = await auth.attempt(data.email, data.password)
+
+        return token
+    }
 }
 
 module.exports = UserController
