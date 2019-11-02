b657a1928fe2779c4321552dbfe4d88bb70142a0
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-11-02 11:14:18 -0300

    fix: store and index users

--- server/app/Controllers/Http/UserController.js
+++ server/app/Controllers/Http/UserController.js
@@ -2,18 +2,18 @@
 
 const User = use('App/Models/User')
 class UserController {
-    async store({ request, response, auth }){
+    async store({ request, auth }){
         const data = request.only([ 'name', 'email', 'password' ])
         const user = await User.create(data)
 
-        const token = await auth.attempt(data.email, data.password)
+        // const token = await auth.attempt(data.email, data.password)
 
-        return token
+        return user
     }
 
     async index({ request }){
         const users = await User.query()
-            .where('team_id', request.team.id)
+            // .where('team_id', request.team.id)
             .fetch()
         
         return users
