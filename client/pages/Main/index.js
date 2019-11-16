e043e1bd8599792159d4cb322884cd052fd4c52f
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-11-16 16:38:06 -0300

    new: page Main

---
+++ client/pages/Main/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// import TeamSwitcher from '~/components/TeamSwitcher';
+// import Projects from '~/components/Projects';
+
+import { Container } from './styles'
+const Main = () => {
+    return (
+        <Container>
+            {/* <TeamSwitcher/> */}
+            {/* <Projects/> */}
+        </Container>
+    );
+}
+
+export default Main;
