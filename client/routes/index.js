262fc7c5ee6c46eae63a26185bfbbcbcdcf90760
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-11-16 16:26:19 -0300

    new: add route index

---
+++ client/routes/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ConnectedRouter } from 'connected-react-router';
+import { Switch } from 'react-router-dom';
+
+import history from './history';
+
+import Private from './private';
+import Guest from './guest';
+
+import Main from '~/pages/Main';
+import SignIn from '~/pages/Auth/SignIn';
+import SignUp from '~/pages/Auth/SignUp';
+
+const Routes = () => (
+    <ConnectedRouter history={history}>
+        <Switch>
+            <Guest path="/signin" component={SignIn} />
+            <Guest path="/signup" component={SignUp} />
+            <Private path="/" exact component={Main} />
+        </Switch>
+    </ConnectedRouter>
+)
+
+export default Routes;
