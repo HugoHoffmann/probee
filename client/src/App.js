c43dfeabee6e7ea51a9397006e92c71a8f622d4a
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-11-16 17:01:41 -0300

    chore: ajust App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import ReduxToastr from 'react-redux-toastr';
+import { Provider } from 'react-redux';
+import Routes from './routes';
+import store from './store';
+import GlobalStyle from './styles/global';
 
-function App() {
-  return (
-    <div className="App">
-      oi
-    </div>
-  );
-}
+const App = () => (
+    <Provider store={store}>
+        <Fragment>
+            <Routes/>
+            <ReduxToastr/>
+            <GlobalStyle/>
+        </Fragment>
+    </Provider>
+)
 
 export default App;
