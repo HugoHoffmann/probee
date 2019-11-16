c5aedcc26ae710025eade7f1cb83edc63ae64520
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-11-16 16:55:09 -0300

    chore: add Components into Main

--- client/pages/Main/index.js
+++ client/pages/Main/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-// import TeamSwitcher from '~/components/TeamSwitcher';
-// import Projects from '~/components/Projects';
+import TeamSwitcher from '~/components/TeamSwitcher';
+import Projects from '~/components/Projects';
 
 import { Container } from './styles'
 const Main = () => {
     return (
         <Container>
-            {/* <TeamSwitcher/> */}
-            {/* <Projects/> */}
+            <TeamSwitcher/>
+            <Projects/>
         </Container>
     );
 }
