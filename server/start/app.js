3170c844c2e001f6b0bed709a428e5606240c62e
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-11-16 21:09:41 -0300

    config: cors

--- server/start/app.js
+++ server/start/app.js
@@ -13,6 +13,7 @@
 const providers = [
   '@adonisjs/framework/providers/AppProvider',
   '@adonisjs/auth/providers/AuthProvider',
+  '@adonisjs/cors/providers/CorsProvider',
   '@adonisjs/bodyparser/providers/BodyParserProvider',
   '@adonisjs/cors/providers/CorsProvider',
   '@adonisjs/lucid/providers/LucidProvider'
