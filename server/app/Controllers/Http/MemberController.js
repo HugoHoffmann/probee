6b1db8c645ebe97b1827d9458336b6cf7b5bb50b
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-11-17 19:08:04 -0300

    feat(index): MemberController

--- server/app/Controllers/Http/MemberController.js
+++ server/app/Controllers/Http/MemberController.js
@@ -1,6 +1,20 @@
 'use strict'
 
+const UserTeam = use('App/Models/UserTeam')
+
 class MemberController {
+    async index({ request }){
+        const members = await UserTeam.query()
+            .where('team_id', request.team.id)
+            .with('user')
+            .fetch()
+        
+            return members
+    }
+
+    async update({request, params}){
+        const teamJoin = await UserTeam.find(params.id)
+    }
 }
 
 module.exports = MemberController
