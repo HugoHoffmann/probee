7367ccac119de35fe58b3a20655553b0ad35559c
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-11-17 19:17:00 -0300

    new: job InvitationEmail,  InviteHook and Model Invite

---
+++ server/app/Jobs/InvitationEmail.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const Mail = use('Mail')
+
+class InvitationEmail {
+  // If this getter isn't provided, it will default to 1.
+  // Increase this number to increase processing concurrency.
+  static get concurrency () {
+    return 1
+  }
+
+  // This is required. This is a unique key used to identify this job.
+  static get key () {
+    return 'InvitationEmail-job'
+  }
+
+  // This is where the work is done.
+  async handle ({ user, team, email }) {
+    await Mail.send(
+      ['emails.invitation'],
+      {
+        team: team.name,
+        user: user.name
+      },
+      message => {
+        message
+          .to(email)
+          .from('hugohoffmann04@gmail.com', 'Hugo')
+          .subject(`Convite para o time ${team.name}`)
+      }
+    )
+  }
+}
+
+module.exports = InvitationEmail
+
