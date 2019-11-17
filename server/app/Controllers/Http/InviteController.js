f4b48fffd580582e6dd304ad3e71f26dc43ee725
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-11-17 19:15:36 -0300

    new: InviteController

---
+++ server/app/Controllers/Http/InviteController.js
@@ -0,0 +1,36 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const Invite = use('App/Models/Invite')
+/**
+ * Resourceful controller for interacting with invites
+ */
+class InviteController {
+
+  /**
+   * Create/save a new invite.
+   * POST invites
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, auth }) {
+     const invites = request.input('invites')
+
+     const data = invites.map(email => ({
+       email,
+       user_id: auth.user.id,
+       team_id: request.team.id
+     }))
+
+     await Invite.createMany(data) 
+
+  }
+
+}
+
+module.exports = InviteController
