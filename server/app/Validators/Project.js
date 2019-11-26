68193504f0a3a2694fd4cc4ba7d4d512e262bb0a
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-11-26 09:08:50 -0300

    chore: validator Project

--- server/app/Validators/Project.js
+++ server/app/Validators/Project.js
@@ -1,15 +1,11 @@
 'use strict'
 
 class Project {
+
   get validateAll(){
     return true
   }
-
-  get rules () {
-    return {
-      title: 'required'
-    }
-  }
+  
 }
 
 module.exports = Project
