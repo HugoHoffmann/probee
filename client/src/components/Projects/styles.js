5a1401be9a327603dcae3977fc8f7912ecba7617
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-11-26 09:12:41 -0300

    chore: add style delete project

--- client/src/components/Projects/styles.js
+++ client/src/components/Projects/styles.js
@@ -1,38 +1,51 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-    flex: 1;
-    padding: 20px;
-    overflow-y: scroll;
-         
-    > header{
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin: 0 0 20px;
-        h1{
-            font-size: 20px;
-        }
-        div{
+  flex: 1;
+  padding: 20px;
+  overflow-y: scroll;
 
-            button{
-                margin-left: 10px;
-            }
-
-            icon{
-                margin-left: 20px;
-                cursor: pointer;
-            }
-        }
+  > header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 0 20px;
+    h1 {
+      font-size: 20px;
     }
+    div {
+      button {
+        margin-left: 10px;
+      }
+
+      svg {
+        margin-left: 10px;
+        cursor: pointer;
+      }
+    }
+  }
 `;
 
 export const Project = styled.div`
-    background: rgba(0, 0, 0, 0.2);
-    border-radius: 5px;
-    margin: 0 0 20px; 
-    padding: 20px;
-    p{
-        font-size: 18x;
+  background: rgba(0, 0, 0, 0.3);
+  border-radius: 5px;
+  margin: 0 0 20px;
+  padding: 20px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  p {
+    font-size: 18x;
+  }
+
+  div {
+
+    svg {
+      margin-left: 10px;
+      cursor: pointer;
     }
+    
+  }
 `;
