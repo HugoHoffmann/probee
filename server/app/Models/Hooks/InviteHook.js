360ab972bf1cf1d03722ee9e663637a3a588c11f
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-11-26 11:58:56 -0300

    fix: invitation email

--- server/app/Models/Hooks/InviteHook.js
+++ server/app/Models/Hooks/InviteHook.js
@@ -1,16 +1,16 @@
 'use strict'
 
-const InviteHook = exports = module.exports = {}
+const InviteHook = (exports = module.exports = {})
 const User = use('App/Models/User')
 const Kue = use('Kue')
 const Job = use('App/Jobs/InvitationEmail')
 
-InviteHook.sendInvitationEmail = async (invite) => {
+InviteHook.sendInvitationEmail = async invite => {
     const { email } = invite 
     const invited = await User.findBy('email', email)
 
     if(invited){
-        await invited.teams().attach(invate.team_id)
+        await invited.teams().attach(invite.team_id)
     }else{
         const user = await invite.user().fetch()
         const team = await invite.team().fetch()
